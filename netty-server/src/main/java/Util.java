395c50ee9fc62c0d4b53c515b18ec38c2a932caf
Михаил Левин <mikelevin@yandex-team.ru>
2020-09-29 21:51:12 +0300

    Netty

---
+++ netty-server/src/main/java/Util.java
@@ -0,0 +1,26 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.stream.ChunkedFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Util {
+    public static void main(String[] args) throws IOException {
+        Bootstrap bootstrap; // config
+        ServerBootstrap serverBootstrap;
+        ByteBuf buf;
+        NioServerSocketChannel srvChannel;
+        ChannelInboundHandlerAdapter in;
+        ChannelOutboundHandlerAdapter out;
+        NioSocketChannel channel;
+        ChunkedFile chunkedFile = new ChunkedFile(new File(""), 8192);
+        // chunkedFile.readChunk(null);
+
+    }
+}
